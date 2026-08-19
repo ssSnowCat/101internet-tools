b6b47c426a996bf395f9eba03f29be0e7f96748e
RomanYa <cnektatop@gmail.com>
2026-08-19 12:01:29 +0300

    Update 101Internet-Tools-2.2.0.user.js

--- 101Internet-Tools-2.2.0.user.js
+++ 101Internet-Tools-2.2.0.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         101Internet Tools — Общая коробка
 // @namespace    https://adviser-new.101internet.ru/
-// @version      2.1.0
+// @version      2.2.0
 // @description  Единая панель инструментов для заявок 101internet. Офлайн-база населённых пунктов РФ.
 // @author       You + ChatGPT
 // @match        https://adviser-new.101internet.ru/orders/*
+// @updateURL    https://raw.githubusercontent.com/ssSnowCat/101internet-tools/main/101Internet-Tools-2.2.0.user.js
+// @downloadURL  https://raw.githubusercontent.com/ssSnowCat/101internet-tools/main/101Internet-Tools-2.2.0.user.js
 // @grant        GM_setClipboard
 // @run-at       document-idle
 // ==/UserScript==
